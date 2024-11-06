d43b04d112845c7257d25d29eb61c0c574289c68
Ananiasnt <netimcunha@gmail.com>
2024-11-06 11:18:11 -0300

    Projeto finalizado

--- src/app.js
+++ src/app.js
@@ -1,25 +1,25 @@
-import express,{ json } from "express";
+import express,{ json, query } from "express";
 import cors from "cors";
 
 const items = [
     {
         id:1,
-        nome: "Maçã",
-        quantidade: 10,
-        tipo: "Fruta"
+        name: "Maçã",
+        quantify: 10,
+        type: "Fruta"
     },
     {
         id:2,
-        nome: "Arroz",
-        quantidade: 2,
-        tipo: "Grão"
+        name: "Arroz",
+        quantify: 2,
+        type: "Grão"
     },
    
     {
         id:3,
-        nome: "Abobrinha",
-        quantidade: 3,
-        tipo: "Legume"
+        name: "Abobrinha",
+        quantify: 3,
+        type: "Legume"
     },
    
 ];
@@ -32,11 +32,11 @@ app.use(json());
 
 app.post("/items",(req,res)=>{
     const item=req.body;
-    if(!item.nome || !item.quantidade || !item.tipo){
-        return res.status(422).send("vc n enviou todas informacoes")
+    if(!item.name || !item.quantify || !item.type){
+        return res.status(422).send("vc não enviou todas informacoes")
     }
     items.forEach(itemDaLista => {
-        if (item.nome.toLowerCase() === itemDaLista.nome.toLowerCase()) {
+        if (item.name.toLowerCase() === itemDaLista.name.toLowerCase()) {
             return res.status(409).send("O item já foi adicionado");
         }
     });
@@ -48,21 +48,35 @@ app.post("/items",(req,res)=>{
     res.status(201).send("criado com sucesso")
 })
 app.get("/items",(req,res)=>{
-    res.send(items)
+    const { type }=req.query;
+    if(type){
+        const itemComType=items.filter(item=>item.type.toLowerCase()===type.toLowerCase())
+        if(itemComType.length
+            ===0
+        ){
+              return res.send([]);
+        }else{
+         return res.send(itemComType);
+        }
+        
+    } 
+    return res.send(items)
 })
 
-app.get("/items/:id",(req,res)=>{
-    const id=req.params.id;
-    const itemComId=items.find(item=>{
-        return item.id===Number(id);
-    })
-    res.send(itemComId)
-    
-})
-/*app.get("/items/:id",(req,res)=>{
-    pra pegar itens especificos por nome
-})*/
 
+
+app.get("/items/:id", (req, res) => {
+    const id = Number(req.params.id);
+
+    if (id <= 0) {
+        return res.status(400).send("Digite um ID válido ou seja acima de 0");
+    }
+    const itemComId = items.find(item => item.id === id);
+    if (!itemComId) {
+        return res.status(404).send("Digite um ID existente na nossa aplicação");
+    }
+    res.send(itemComId);
+});
 
 
 app.listen(5000,()=>{
